6b118985d13f0bc254f6a50eab0476a54d768728
ytytryg <92774174+ytytryg@users.noreply.github.com>
2021-10-19 13:19:00 +0800

    Delete mmdet/mmdet/models directory

--- mmdet/mmdet/models/dense_heads/__init__.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .anchor_head import AnchorHead
-from .rpn_head import RPNHead
-
-
-__all__ = [
-   'AnchorHead', 'RPNHead'
-]
--- mmdet/mmdet/models/detectors/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .base import BaseDetector
-from .cascade_rcnn import CascadeRCNN
-from .mask_rcnn import MaskRCNN
-from .two_stage import TwoStageDetector
-
-__all__ = [
-  'BaseDetector', 'TwoStageDetector',  'CascadeRCNN', 'MaskRCNN'
-]
