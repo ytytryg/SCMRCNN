5ee4e81f69ab3e7bf49c22ccf0f283ab142a1be4
ytytryg <92774174+ytytryg@users.noreply.github.com>
2021-10-19 13:03:45 +0800

    Add files via upload

---
+++ mmdet/datasets/__init__.py
@@ -0,0 +1,17 @@
+# Copyright (c) OpenMMLab. All rights reserved.
+from .builder import DATASETS, PIPELINES, build_dataloader, build_dataset
+from .coco import CocoDataset
+from .custom import CustomDataset
+from .dataset_wrappers import (ClassBalancedDataset, ConcatDataset,
+                               MultiImageMixDataset, RepeatDataset)
+from .samplers import DistributedGroupSampler, DistributedSampler, GroupSampler
+from .utils import (NumClassCheckHook, get_loading_pipeline,
+                    replace_ImageToTensor)
+
+__all__ = [
+    'CustomDataset','CocoDataset', 'GroupSampler', 'DistributedGroupSampler',
+    'DistributedSampler', 'build_dataloader', 'ConcatDataset', 'RepeatDataset',
+    'ClassBalancedDataset',  'DATASETS', 'PIPELINES',
+    'build_dataset', 'replace_ImageToTensor', 'get_loading_pipeline',
+    'NumClassCheckHook', 'CocoPanopticDataset', 'MultiImageMixDataset'
+]
